cca834d2bdba375cd433a43a95e1f085c7ed34d4
Fredrick Gonu <joeselorm@gmail.com>
2023-03-23 21:15:06 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+typedef struct checker
+{
+	char *type;
+	void (*f)();
+} checker;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
